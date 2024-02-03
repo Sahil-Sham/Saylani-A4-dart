8c0441f747714000962326b580119630dffa0f5c
Sahil-Sham <sahilsham173@gmail.com>
2024-02-03 19:22:13 +0500

    Q14 complete

---
+++ Q14.dart
@@ -0,0 +1,14 @@
+// Q14.	Write a program to make such a pattern like a right angle triangle with the number increased by 1 using loop.. The pattern like :
+import 'dart:io';
+
+void main(){
+  int count=1;
+  for (var i = 0; i <5; i++) {
+    for (var u = 0; u <i ; u++) {
+ 
+      stdout.write("$count");
+          count++; 
+    }
+    print("");
+  }
+}
