9b1cf93e7df29325b03ae2b0a1f412f0992f1f44
Sahil-Sham <sahilsham173@gmail.com>
2024-02-04 18:22:44 +0500

    Q17 complete

---
+++ Q17.dart
@@ -0,0 +1,20 @@
+// Q17.	Write a program that asks the user for their email and password. If the email and password match a predefined set of credentials, print "User login successful." Otherwise, keep asking for the email and password until the correct credentials are provided.
+import 'dart:io';
+
+void main(){
+
+  bool islogin =false;
+  while (islogin==false) {
+     stdout.write("Enter Email:");
+  var email=(stdin.readLineSync()!);
+  stdout.write("Enter Password:");
+  var password=stdin.readLineSync()!;
+    if (email=="sahil@gmail.com" && password=="123456") {
+    print("Login Successfully");
+    islogin=true;
+  }else{
+    print("Login failed");
+  }
+  }
+  
+  }
---
+++ Q5.dart
@@ -0,0 +1,12 @@
+// Q5.	Write a program that calculates the sum of all the digits in a given number using a while loop.
+import 'dart:io';
+
+void main(){
+  stdout.write("Enter the digits:");
+  String digit=stdin.readLineSync()!;
+  int number=int.parse(digit);
+  int numbers=number;
+  while (numbers==true) {
+   number++;
+  }
+}
