8febec8c9931dd88b04a3bde2d538d33b9c359ca
Sahil-Sham <sahilsham173@gmail.com>
2024-02-07 13:17:40 +0500

    Q6 complete

--- Q6.dart
+++ Q6.dart
@@ -4,6 +4,13 @@ void main(){
   stdout.write("Enter the Element:");
 String element =stdin.readLineSync()!;
 
-List<String> numberstring=element.split(" ");
-List<int> numbers=numberstring.map((String) => int.parse(String)).toList();
+List<int> number=element.split(",").map((String) => int.parse(String)).toList();
+ int value=number[0];
+for (var i = 1; i < number.length; i++) {
+  if (number[i]>value) {
+    value=number[i];
+  }
+}
+
+print("The largest element:$value");
 }
